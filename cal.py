e168dcb64dc23fd1a1e934f27a7b18668834f7aa
mulosh <joseph.barasa321@gmail.com>
2026-04-06 13:10:48 +0300

    calculator

---
+++ cal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    result = a + b
+    return result
+  
+def sub(a, b):
+    result = a - b
+    return result
+
+def mul(a, b):
+    result = a * b
+    return result
+
+def div(a, b):
+    result = a / b
+    return result
---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from cal import add, sub, mul, div
+
+a = int(input("enter the first value: "))
+b = int(input("enter the second value: "))
+
+print("addittion: ", add(a, b))
+print("subtract: ", sub(a, b))
+print("multiply: ", mul(a, b))
+print("division: ", div(a, b))
